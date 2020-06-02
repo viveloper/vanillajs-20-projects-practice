92c1d52f8642fe576f20fc8cd8c0722b24060cf6
viveloper <ucsik1234@gmail.com>
2020-06-02 15:13:12 +0900

    updated movie-seat-booking-02

--- movie-seat-booking-02/script.js
+++ movie-seat-booking-02/script.js
@@ -4,8 +4,6 @@ const count = document.querySelector('#count');
 const total = document.querySelector('#total');
 const movieSelect = document.querySelector('#movie');
 
-let ticketPrice = +movieSelect.value;
-
 container.addEventListener('click', (e) => {
   if (
     e.target.classList.contains('seat') &&
@@ -17,13 +15,56 @@ container.addEventListener('click', (e) => {
 });
 
 movieSelect.addEventListener('change', (e) => {
-  ticketPrice = +e.target.value;
   updateSelectedCount();
 });
 
+loadData();
+updateSelectedCount();
+
 function updateSelectedCount() {
-  const selectedSeatsCount = document.querySelectorAll('.row .seat.selected')
-    .length;
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+  const ticketPrice = movieSelect.value;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+
+  saveData();
+}
+
+function saveData() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsIndex = [...selectedSeats].map((selectedSeat) =>
+    [...seats].findIndex((seat) => seat === selectedSeat)
+  );
+  const selectedMovieIndex = movieSelect.selectedIndex;
+
+  localStorage.setItem(
+    'selectedSeatsIndex',
+    JSON.stringify(selectedSeatsIndex)
+  );
+  localStorage.setItem(
+    'selectedMovieIndex',
+    JSON.stringify(selectedMovieIndex)
+  );
+}
+
+function loadData() {
+  const selectedMovieIndex = parseInt(
+    localStorage.getItem('selectedMovieIndex')
+  );
+  if (selectedMovieIndex) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+
+  const selectedSeatsIndex = JSON.parse(
+    localStorage.getItem('selectedSeatsIndex')
+  );
+  if (selectedSeatsIndex) {
+    seats.forEach((seat, index) => {
+      if (selectedSeatsIndex.includes(index)) {
+        seat.classList.add('selected');
+      }
+    });
+  }
 }
